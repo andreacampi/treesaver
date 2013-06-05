1ad1a1c4618f065e2904cfe448719e42a9d8942e
Andrea Campi <andrea.campi@zephirworks.com>
2013-06-06 00:26:27 +0200

    Improve lightbox tests: * test activate(); * set the lightbox node up in a similar way to what Resources and StateManager do.

--- test/lightbox.js
+++ test/lightbox.js
@@ -1,6 +1,8 @@
 goog.require('treesaver.ui.LightBox');
 
 $(function() {
+  var container;
+
   module('lightbox', {
     setup: function () {
       // Create an HTML tree for test data
@@ -10,10 +12,23 @@ $(function() {
         url: 'assets/chrome.html',
         success: function (data) {
           if (data) {
-            var container = document.createElement('div');
-            document.body.appendChild(container);
-            container.className = 'testonly container';
-            container.innerHTML = data;
+            var testonly = document.createElement('div');
+            document.body.appendChild(testonly);
+            testonly.className = 'testonly container';
+
+            container = document.createElement('div');
+
+            var body = treesaver.resources.extractBody(data);
+            if (body) {
+              var div = document.createElement('div');
+              div.innerHTML = body;
+              treesaver.array.toArray(div.childNodes).forEach(function(child) {
+                if (/^div$/i.test(child.nodeName)) {
+                  container.appendChild(child);
+                }
+              });
+              treesaver.dom.clearChildren(div);
+            }
           }
         }
       });
@@ -24,7 +39,12 @@ $(function() {
   });
 
   test('Construction', function() {
-    var lb = new treesaver.ui.LightBox($('.testonly .lightbox')[0]);
+    var node = $('.lightbox', container)[0],
+        div = document.createElement('div');
+    document.body.appendChild(div);
+    div.appendChild(node)
+    var lb = new treesaver.ui.LightBox(node);
+    div.removeChild(node)
 
     // Sanity check for now, will fill in real tests later
     ok(lb, 'Object created');
@@ -32,4 +52,23 @@ $(function() {
     ok(lb.fits({ w: Infinity, h: Infinity}), 'Fits');
     ok(lb.meetsRequirements(), 'Requirements');
   });
+
+  test('activate', function() {
+    var node = $('.lightbox', container)[0],
+        div = document.createElement('div');
+    document.body.appendChild(div);
+    div.appendChild(node);
+    var lb = new treesaver.ui.LightBox(node);
+    div.removeChild(node);
+
+    var node = lb.activate();
+
+    ok(node, 'Returns a node');
+    ok(lb.active, 'Lightbox is active');
+    ok(lb.container, 'Container is present');
+    ok(lb.container.parentNode === node, 'Container is a child of the node');
+    ok(node.parentNode === null, 'Node is not attached to the document');
+    ok(lb.fits({ w: Infinity, h: Infinity}), 'Fits');
+  });
+
 });
